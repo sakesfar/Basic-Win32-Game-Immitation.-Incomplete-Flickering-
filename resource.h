f2217a7cb36fa286c59c0a4c97aaefce432231e1
sakesfar <121855106+sakesfar@users.noreply.github.com>
2023-01-03 16:48:09 +0600

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+// Next default values for new objects
+// #define IDB_PLAYER  105
+#define IDB_PLAYER  105
+#define IDB_BALL 106
+#define IDB_FIELD 107
+#define IDB_OPPONENT 108
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
